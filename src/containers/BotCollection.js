ff4d89ee86a78102242798242e02567edc67655d
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-10-16 11:44:46 -0500

    submission for deadline

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,19 +2,35 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
-
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  //initialize component with props data of bots
+  constructor(props) {
+    super(props);
+    this.state = {
+      bots: props.bots
+    };
   }
 
-};
+  render() {
+    //check if data got passed through
+    //console.log("Current BotsCollection State = ", this.state.bots);
+
+    //pass data to BotCard to render individual cards, map through array
+    return (
+      <div className="ui four column grid">
+        <div className="row">
+          {this.state.bots.map(bot => {
+            return (
+              <BotCard
+                key={bot.id}
+                bot={bot}
+                handleClick={this.props.handleClick}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default BotCollection;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,49 @@
 import React from "react";
+import BotCollection from "./BotCollection";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    bots: []
+  };
+
+  componentDidMount() {
+    this.fetchBots();
+  }
+
+  fetchBots() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(response => response.json())
+      .then(botsData => {
+        //get all bot data via fetch and check data
+        //console.log("Fetch bots data = ", botsData);
+
+        //set to state
+        this.setState({
+          bots: botsData
+        });
+      });
+  }
+
+  handleClick = e => {
+    console.log("Handle Click event hit ", e);
+    //render bots army
+  };
 
   render() {
+    //pass all bots to BotCollection only if bots array is > 0
+    //console.log("Current BotsPage State", this.state.bots);
     return (
       <div>
-        {/* put your components here */}
+        {this.state.bots.length > 0 && (
+          <BotCollection
+            bots={this.state.bots}
+            handleClick={this.handleClick}
+          />
+        )}
       </div>
     );
   }
-
 }
 
 export default BotsPage;

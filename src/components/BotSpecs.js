ab7d60d9e807e489ba8749e13cea3b4108864eef
Anais Ahmed <anaismahmed@uchicago.edu>
2019-10-16 11:09:41 -0500

    finished

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -60,19 +60,13 @@ const BotSpecs = props => {
             </div>
             <button
               className="ui button fluid"
-              onClick={() =>
-                console.log('connect this to a function that shows all bots')
-              }
+              onClick={props.viewAll}
             >
               Go Back
             </button>
             <button
               className="ui button fluid"
-              onClick={() =>
-                console.log(
-                  "connect this to a function that adds this bot to your bot army list"
-                )
-              }
+              onClick={() => props.addBot(bot)}
             >
               Enlist
             </button>
--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,21 +1,44 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from '../components/BotSpecs';
 
 class BotCollection extends React.Component {
+  constructor(props){
+	  super(props);
+	  this.state = {
+		  showSpecs: false,
+		  bot: {}
+	  }
+  }
+  
 
   handleClick = (bot) => {
-		  this.props.addBot(bot)
+	  this.setState({showSpecs: true, bot: bot})
+  }
+
+  viewAll = () => {
+	  this.setState({showSpecs: false, bot: {}})
+  }
+
+  addBot = (bot) => {
+	  this.props.addBot(bot);
+	  this.setState({showSpecs: false, bot: {}})
   }
 
   render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
+	if(this.state.showSpecs === false){
+  	  return (
+  	    <div className="ui four column grid">
+    	  	<div className="row">
     		  {this.props.bots.map(bot => <BotCard handleClick={this.handleClick} key={bot.id} bot={bot}/>)}
     		  Collection of all bots
     		</div>
-  	  </div>
-  	);
+  	    </div>
+	  );
+	}
+	else {
+		return (<BotSpecs bot={this.state.bot} viewAll={this.viewAll} addBot={this.addBot}/>)
+	}
   }
 
 };

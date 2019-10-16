65d4db4c2ef850317b65ebc165473d525eb1c486
Cody <lawdermiltcody@gmail.com>
2019-10-16 11:44:43 -0500

    submission for deadline

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,58 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    botStateData: [],
+    botClickID: []
+  };
+
+  botDataFetching() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(res => res.json())
+      .then(botData => {
+        // console.log("this is my data", botData);
+        this.setState({
+          botStateData: botData
+        });
+      });
+  }
+  componentDidMount() {
+    this.botDataFetching();
+  }
+
+  handleClick = bot => {
+    // console.log("this is when i click a bot, what id i'll be given", botID);
+    this.setState({
+      botClickID: bot
+    });
+  };
 
   render() {
+    // console.log("My current State after fetching", this.state.botStateData);
+    // console.log(
+    //   "My current StateID after clicking a bot",
+    //   this.state.botClickID
+    // );
     return (
       <div>
-        {/* put your components here */}
+        <div>
+          <YourBotArmy
+            bot={this.state.botClickID}
+            handleClick={this.handleClick}
+          />
+        </div>
+        <div>
+          <BotCollection
+            botData={this.state.botStateData}
+            handleClick={this.handleClick}
+          />
+        </div>
       </div>
     );
   }
-
 }
 
 export default BotsPage;
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -2,21 +2,35 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+  constructor(props) {
+    super(props);
 
-  render(){
+    // save data in state
+    this.state = {
+      bot: props.bot
+    };
+  }
+  handleAddBot = () => {
+    const bot = this.stae.bot.slice;
+    let size = bot.length + 1;
+    bot.push(size);
+    this.setState({
+      bot: bot
+    });
+  };
+
+  render() {
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
             Your Bot Army
+            <BotCard bot={this.props.bot} />
           </div>
         </div>
       </div>
     );
   }
-  
-};
+}
 
 export default YourBotArmy;

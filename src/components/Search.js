a9c871c0d25e38a6876a32d476013c22c9d62bec
Jason <jas0nle@yahoo.com>
2019-10-16 11:43:56 -0500

    submission for deadline

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Search = (props) => {
     return (
         <div className="search">
-        <label>Filter Bots:</label>
-        <input type="text" onChange={event => props.onSearch(event.target.value)}>
-        </input>
+            <label>Filter Bots:</label>
+            <input type="text" onChange={event => props.onSearch(event.target.value)}>
+            </input>
         </div>
     )
 }
